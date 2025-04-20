1e7b3da451cfa219cc79c98ec1bd9133854b392c
Scott Browning <scottybrowning12345@gmail.com>
2025-04-19 22:28:44 -0400

    added JTextField

--- src/InvoiceJFrame.java
+++ src/InvoiceJFrame.java
@@ -1,2 +1,18 @@
-public class InvoiceJFrame {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.util.List;
+
+    public class InvoiceJFrame  extends JFrame {
+    private JTextField customerNameField = new JTextField(20);
+    private JTextField customerAddressField = new JTextField(20);
+    private JTextField productNameField = new JTextField(10);
+    private JTextField productPriceField = new JTextField(5);
+    private JTextField quantityField = new JTextField(5);
+    private JTextArea outputArea = new JTextArea(15, 40);
+
+
+    private Invoice invoice;
+
+//appstuff
 }
